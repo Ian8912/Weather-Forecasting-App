ec54a8fce0378fd8e7f122979d2ac2ae8d2e4167
Anthony <ajr203@txstate.edu>
2024-10-01 14:03:42 -0500

    Added imports to load API key and created base url for API call

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import json
+import os
+import requests
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 CORS(app)
+
+load_dotenv()
+API_KEY = os.getenv('OPENWEATHER_API_KEY')
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 # weather route with placeholder data, modifiy later to fetch API data
 @app.route('/weather', methods=['GET'])
