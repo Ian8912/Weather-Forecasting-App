362c574fcfdfc28074df59895caa794ed220ee46
Ethan <lyg23@txstate.edu>
2024-09-30 21:25:28 -0500

    Created class for handling test cases

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -3,3 +3,18 @@ from flask_testing import TestCase
 
 app = Flask(__name__)
 
+@app.route('/')
+def landing_page():
+    return render_template('landing_page.html')
+
+class TestLandingPage(TestCase):
+    def create_app(self):
+        # Configures Flask app for testing purposes
+        app.config['TESTING'] = True
+        return app
+    
+    def test_landing_page_renders(self):
+        # Simulate a request to the landing page
+        response = self.client.get('/')
+        self.assert200(response)  # Assert that page loads with status code 200
+        self.assert_template_used('landing_page.html')
