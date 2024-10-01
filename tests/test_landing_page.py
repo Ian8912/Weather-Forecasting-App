bcb8f09783efe4cf84c8c19da48dbaa346d6bccd
Ethan <lyg23@txstate.edu>
2024-09-30 21:42:11 -0500

    Changed placeholder HTML name to current HTML in use

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
-    return render_template('landing_page.html')
+    return render_template('index.html')
 
 class TestLandingPage(TestCase):
     def create_app(self):
@@ -17,7 +17,7 @@ class TestLandingPage(TestCase):
         # Simulate a request to the landing page
         response = self.client.get('/')
         self.assert200(response)  # Assert that page loads with status code 200
-        self.assert_template_used('landing_page.html')
+        self.assert_template_used('index.html')
 
 if __name__ == '__main__':
     import unittest
