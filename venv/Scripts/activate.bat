26c69006efb30975123f195f0721862770385285
Anthony <ajr203@txstate.edu>
2024-10-15 15:56:48 -0500

    Connected feature display component to app.jsx

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ian89\OneDrive\Documents\TXST\Software Engineering\Weather app\gorn_weather_app\venv
+set VIRTUAL_ENV=C:\Users\pengu\Desktop\gorn_weather_app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
