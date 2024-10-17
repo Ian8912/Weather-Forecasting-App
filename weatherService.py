93fa5d9e5a0686fd6d9c34d6eada1e331f1eb768
TajTelesford <TajTelesford@gmail.com>
2024-10-17 17:51:47 -0500

    SCRUM-120 Basic route set up & added to weather service

--- weatherService.py
+++ weatherService.py
@@ -8,6 +8,7 @@ load_dotenv()
 API_KEY = os.getenv('OPENWEATHER_API_KEY')
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct'
+FORECAST_URL = 'https://api.openweathermap.org/data/3.0/onecall'
 
 # Function to fetch weather data by coordinates
 def fetch_weather_data(lat, lon):
@@ -28,3 +29,14 @@ def fetch_geo_data(city):
         return response.json()
     except requests.exceptions.RequestException as e:
         return {"error": "Failed to fetch city data: " + str(e)}
+# Fetehes forecast data
+def fetch_forecast_data(lat, lon):
+    forecast_url = f"{FORECAST_URL}?lat={lat}&lon={lon}&appid={API_KEY}"
+    try:
+        res = requests.get(forecast_url, timeout=10)
+        res.raise_for_status()
+        return res.json()
+    except requests.exceptions.RequestException as e:
+        print("ERROR")
+        return {"error": "Failed to find forecast for this city" + str(e)}
+    
