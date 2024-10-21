94ee21504d044dd9f833c74bbc4e5136f7473c6c
Anthony <ajr203@txstate.edu>
2024-10-21 12:53:57 -0500

    SCRUM-97 Added translation hook to FeatureForm.jsx

--- src/components/FeatureForm.jsx
+++ src/components/FeatureForm.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const FeatureForm = () => {
+  const { t } = useTranslation(); // Translation function
   return (
    <div className="container mx-auto">
-    <h3 className="text-3xl font-bold text-center mb-8 dark:text-[#cbd5e1]">Feedback Form</h3>
+    <h3 className="text-3xl font-bold text-center mb-8 dark:text-[#cbd5e1]">{t('Feedback Form')}</h3>
     <form onSubmit={handleSubmit}>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#cbd5e1]" htmlFor="name">
-          Name
+        {t('Name')}
         </label>
         <input
           id="name"
@@ -20,7 +22,7 @@ const FeatureForm = () => {
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#cbd5e1]" htmlFor="email">
-          Email
+        {t('Email')}
         </label>
         <input
           id="email"
@@ -33,7 +35,7 @@ const FeatureForm = () => {
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#cbd5e1]" htmlFor="feedback">
-          Feedback
+        {t('Feedback')}
         </label>
         <textarea
           id="feedback"
@@ -47,7 +49,7 @@ const FeatureForm = () => {
         type="submit"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline dark:bg-[#312e81] dark:text-[#cbd5e1]"
       >
-        Submit
+        {t('Submit')}
       </button>
     </form>
   </div>
