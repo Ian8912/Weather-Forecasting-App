ee843d932da5d13fe2b109451fa7f306d0295cce
TajTelesford <TajTelesford@gmail.com>
2024-10-21 18:22:53 -0500

    SCRUM-141 Changes to how Forecast data gets accessed in frontend

--- src/components/ForcastCard.jsx
+++ src/components/ForcastCard.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
-export const ForcastCard = (day) => {
+export const ForcastCard = ({ 
+  date,
+  maxTemp,
+  minTemp,
+  precipitationDescription,
+  windDescription,
+  windSpeed
+ }) => {
   return (
     <div className="weather-card">
-        <h3 className="text-xl font-bold">{day.date}</h3>
+        <h3 className="text-xl font-bold">{date}</h3>
         <p className="text-lg">{day.condition}</p>
         <img src={day.icon} alt={day.condition} className="mx-auto" />
-        <p className="text-lg">High: {day.highTemp}°F / {day.highTempC}°C</p>
-        <p className="text-lg">Low: {day.lowTemp}°F / {day.lowTempC}°C</p>
+        <p className="text-lg">High: {maxTemp[1]}°F / {maxTemp[0]}°C</p>
+        <p className="text-lg">Low: {minTemp[1]}°F / {minTemp[0]}°C</p>
         <p className="text-lg">Humidity: {day.humidity}%</p>
-        <p className="text-lg">Wind Speed: {day.windSpeed} m/s</p>
+        <p className="text-lg">Wind Speed: {windSpeed} m/s</p>
+        <p className="text-lg">Wind Description: {windDescription} m/s</p>
+        <p className="text-lg">Precipitation: {precipitationDescription} m/s</p>
       </div>
   )
 }
