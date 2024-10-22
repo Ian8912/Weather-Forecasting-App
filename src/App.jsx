c25f2318bfe7e12ef23ed8f66df9b846ff87330b
Nicholas Esteves <scr145@txstate.edu>
2024-10-22 03:44:08 -0500

    SCRUM-143 Added timer for feedback form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'
+import './App.css';
 import CoordinateInputCard from './components/CoordinateInputCard';
 import FeatureDisplaySection from './components/FeatureDisplaySection';
 import FeatureForm from './components/FeatureForm';
@@ -26,11 +26,13 @@ function App() {
   });
   
   const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(false); // Loading state to track data fetching
-  const [city, setCity] = useState(''); // Update state in app to handle city input
-  const [suggestions, setSuggestions] = useState([]) // City suggestions
+  const [loading, setLoading] = useState(false); 
+  const [city, setCity] = useState(''); 
+  const [suggestions, setSuggestions] = useState([]); 
   const [errorMessage, setErrorMessage] = useState(null);
   const [isModalVisible, setModalVisible] = useState(false);
+  const [timerId, setTimerId] = useState(null);
+  const [hasModalBeenShown, setHasModalBeenShown] = useState(false); // Track if modal has been shown
 
   const fetchWeatherByCoords = (lat, lon) => {
     setLoading(true);
@@ -38,7 +40,7 @@ function App() {
       .then((response) => {
         const handledResponse = errorService.handleApiError(response, 'Failed to fetch weather data.');
         if (handledResponse.errorMessage) {
-          setErrorMessage(handledResponse.errorMessage); // Set the standardized error message
+          setErrorMessage(handledResponse.errorMessage);
         }
         return handledResponse.json();
       })
@@ -52,14 +54,13 @@ function App() {
         setLoading(false);
       });
   };
-  
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          console.log("User's location:", latitude, longitude);  // Debugging to check location
-          fetchWeatherByCoords(latitude, longitude);  // Fetch data using coordinates
+          fetchWeatherByCoords(latitude, longitude);  
         },
         (error) => {
           setErrorMessage('Unable to access location. Please enter a city manually.');
@@ -70,21 +71,14 @@ function App() {
     }
   }, []);
 
-  
-
-  // Fetch weather data on form submit
   const handleWeatherSubmit = (e) => {
-    e.preventDefault();  // Prevent page reload on form submit
-  
+    e.preventDefault();  
     if (!city) {
-      alert('Please enter a city');  // Ensure city is entered
+      alert('Please enter a city');
       return;
     }
-  
-    setLoading(true);  // Start loading spinner
-    console.log("Fetching weather data for:", city);  // Debugging
-  
-    // Fetch weather data for the selected or entered city
+
+    setLoading(true);  
     fetch(`http://localhost:5000/weather?city=${city}`)
       .then((response) => {
         if (!response.ok) {
@@ -96,30 +90,27 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        console.log('Weather data for submitted city:', data);  // Debugging
-        setWeatherData(data);  // Update state with fetched weather data
-        setLoading(false);  // Stop loading spinner
-        setSuggestions([]);  // Clear suggestions after selecting
-        setErrorMessage(null);  // Clear error message if successful
+        setWeatherData(data);  
+        setLoading(false);  
+        setSuggestions([]);  
+        setErrorMessage(null);  
       })
       .catch((error) => {
-        console.error(`Error fetching weather data for ${city}:`, error);
-        setLoading(false);  // Stop loading spinner on error
-        setErrorMessage(error.message);  // Set error message
+        setLoading(false);  
+        setErrorMessage(error.message);  
       });
   };
 
-  // Fetch city suggestions as the user types
   const handleCityChange = (e) => {
     const cityInput = e.target.value;
     setCity(cityInput);
 
-    if (cityInput.length > 2) { // Fetch suggestions after 3 characters
+    if (cityInput.length > 2) {
       fetch(`http://localhost:5000/city-suggestions?city=${cityInput}`)
         .then((response) => response.json())
         .then((data) => {
           if (data.cities) {
-            setSuggestions(data.cities); // Set city suggestions
+            setSuggestions(data.cities); 
           } else {
             setSuggestions([]);
           }
@@ -128,34 +119,67 @@ function App() {
           console.error('Error fetching city suggestions:', error);
         });
     } else {
-      setSuggestions([]); // Clear suggestions if input is too short
+      setSuggestions([]); 
     }
   };
 
-  // Handle city selection from suggestions
   const handleCitySelect = (selectedCity) => {
-    const { lat, lon, name, state, country } = selectedCity; // Get lat, lon, name, state, and country
-    setCity(name); // Set the selected city name for display purposes
-    setSuggestions([]); // Clear suggestions after selection
-    
-    // Store state and country locally so you can display them later
+    const { lat, lon, name, state, country } = selectedCity;
+    setCity(name); 
+    setSuggestions([]); 
+
     const locationDetails = { name, state, country };
 
-    // Fetch weather data using lat and lon
     setLoading(true);
     fetch(`http://localhost:5000/weather?lat=${lat}&lon=${lon}`)
       .then((response) => response.json())
       .then((data) => {
-        setWeatherData({ ...data, ...locationDetails }); // Merge weather data with location details
-        setLoading(false); // Stop loading after data is fetched
+        setWeatherData({ ...data, ...locationDetails }); 
+        setLoading(false); 
       })
       .catch((error) => {
         console.error('Error fetching weather data:', error);
-        setLoading(false); // Stop loading even if there's an error
+        setLoading(false); 
       });
   };
 
-      // Handle form input changes for feedback form
+  const handleOpenModal = () => {
+    setModalVisible(true);
+    setHasModalBeenShown(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
+  useEffect(() => {
+    const resetTimer = () => {
+      clearTimeout(timerId);
+      if (!hasModalBeenShown) {
+        const newTimerId = setTimeout(() => {
+          handleOpenModal();
+        }, 300000); 
+        setTimerId(newTimerId);
+      }
+    };
+
+    window.addEventListener('mousemove', resetTimer);
+    window.addEventListener('keydown', resetTimer);
+    window.addEventListener('click', resetTimer);
+
+    const initialTimerId = setTimeout(() => {
+      handleOpenModal();
+    }, 300000);
+    setTimerId(initialTimerId);
+
+    return () => {
+      clearTimeout(initialTimerId);
+      window.removeEventListener('mousemove', resetTimer);
+      window.removeEventListener('keydown', resetTimer);
+      window.removeEventListener('click', resetTimer);
+    };
+  }, [hasModalBeenShown, timerId]);
+
   const handleFeedbackChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -164,27 +188,16 @@ function App() {
     });
   };
 
-  // Handle feedback form submission
   const handleFeedbackSubmit = (e) => {
     e.preventDefault();
     console.log('Submitting feedback form:', formData);
-    // Handle form submission logic for feedback form here (e.g., send data to the server)
   };
 
-  const handleOpenModal = () => {
-    setModalVisible(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalVisible(false);
-  };
-
-  // Define the inline function to render weather data
   const RenderWeatherData = ({ weatherData }) => {
     if (!weatherData) {
       return <p>No weather data available. Please enter a city to check the weather.</p>;
     }
-  
+
     return (
       <section className="py-8">
         <div className="container mx-auto text-center">
@@ -195,7 +208,7 @@ function App() {
             <p className="text-lg">Humidity: {weatherData.humidity}%</p>
             <p className="text-lg">Wind Speed: {weatherData.wind_speed} m/s</p>
             <p className="text-lg">
-              UV Index: {weatherData.uv_index || 'N/A'} {/* Add a fallback for UV Index */}
+              UV Index: {weatherData.uv_index || 'N/A'}
             </p>
             <p className="text-lg">Air Quality: {weatherData.air_quality} AQI</p>
           </div>
@@ -213,29 +226,23 @@ function App() {
         <nav className="p-6 bg-blue-600 text-white dark:bg-[#312e81] dark:text-[#cbd5e1]">
           <div className="container mx-auto flex justify-between items-center">
             <h1 className="text-2xl font-bold">WeatherLink</h1>
-  
-            {/* Hamburger Menu for Mobile */}
             <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden text-white focus:outline-none">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xlmns="https://www.w3.org/2000/svg">
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
             </button>
-  
-            {/* Links for Larger Screens */}
             <ul className="hidden md:flex space-x-6">
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Home</a></li>
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Features</a></li>
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Contact</a></li>
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Log in</a></li>
               <li>
-                <button onClick={() => setDarkMode(!darkMode)} 
-                  className="ml-1 px-2 py-1 text-sm bg-blue-500 text-white dark:bg-[#1e1b4b] dark:text-[#cbd5e1] rounded-lg">
+                <button onClick={() => setDarkMode(!darkMode)} className="ml-1 px-2 py-1 text-sm bg-blue-500 text-white dark:bg-[#1e1b4b] dark:text-[#cbd5e1] rounded-lg">
                   {darkMode ? 'Light Mode' : 'Dark Mode'}
                 </button>
               </li>
             </ul>
           </div>
-  
           {menuOpen && (
             <ul className="md:hidden flex flex-col space-y-4 mt-4 text-center">
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Home</a></li>
@@ -243,39 +250,34 @@ function App() {
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Contact</a></li>
               <li><a href="#" className="hover:bg-blue-700 dark:hover:bg-[#1e1b4b] px-3 py-2 rounded transition-colors duration-200">Log in</a></li>
               <li>
-                <button onClick={() => setDarkMode(!darkMode)} 
-                  className="ml-1 px-2 py-1 bg-blue-500 text-white dark:bg-[#1e1b4b] dark:text-[#cbd5e1] rounded-lg">
+                <button onClick={() => setDarkMode(!darkMode)} className="ml-1 px-2 py-1 bg-blue-500 text-white dark:bg-[#1e1b4b] dark:text-[#cbd5e1] rounded-lg">
                   {darkMode ? 'Light Mode' : 'Dark Mode'}
                 </button>
               </li>
             </ul>
           )}
         </nav>
-  
+
         {/* Weather Form Section */}
         <header className="bg-blue-500 dark:bg-[#1e1b4b] dark:text-[#cbd5e1] text-white py-24 text-center">
           <h2 className="text-4xl font-bold">Get the Latest Weather Updates</h2>
           <p className="mt-4 text-lg">Enter a city name to get current weather updates.</p>
-          <>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          </>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           <form onSubmit={handleWeatherSubmit} className="mt-8">
             <div className="relative inline-block w-full max-w-sm">
               <input
                 type="text"
-                value={city}  // Bind to `city` state
-                onChange={handleCityChange}  // Update `city` as user types
+                value={city}
+                onChange={handleCityChange}
                 placeholder="Enter city name"
                 className="text-black w-full px-3 py-2 border rounded text-sm"
               />
-
-              {/* Suggestions dropdown */}
               {suggestions.length > 0 && (
                 <ul className="absolute left-0 right-0 z-10 mt-1 bg-white shadow-lg border rounded text-left">
                   {suggestions.map((suggestion, index) => (
                     <li
                       key={index}
-                      onClick={() => handleCitySelect(suggestion)}  // Handle city selection from suggestions
+                      onClick={() => handleCitySelect(suggestion)}
                       className="cursor-pointer p-2 bg-blue-50 dark:bg-[#312e81] text-gray-800 dark:text-white hover:bg-gray-400 dark:hover:bg-gray-600"
                     >
                       {suggestion.name}, {suggestion.state || ''} ({suggestion.country})
@@ -284,26 +286,14 @@ function App() {
                 </ul>
               )}
             </div>
-
-            {/*<button type="submit" className="ml-4 px-6 py-3 bg-blue-700 dark:bg-[#312e81] dark:text-[#cbd5e1] hover:bg-blue-800 rounded-lg">
-              Check Weather Now
-            </button>*/}
           </form>
         </header>
-  
-        {/* Conditional rendering for loading spinner and weather data */}
-    <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : weatherData ? (
-        <RenderWeatherData weatherData={weatherData} />
-      ) : (
-        <p className="text-center">No weather data available.</p>
-      )}
-    </>
 
-      <CoordinateInputCard />
-  
+        {/* Conditional rendering for loading spinner and weather data */}
+        {loading ? <LoadingSpinner /> : weatherData ? <RenderWeatherData weatherData={weatherData} /> : <p className="text-center">No weather data available.</p>}
+
+        <CoordinateInputCard />
+
         {/* Features Section */}
         <FeatureDisplaySection />
 
