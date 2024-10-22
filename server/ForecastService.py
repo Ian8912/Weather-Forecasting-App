83981cd9e4bc14776a36c6494b30c3efbbd4bfde
TajTelesford <TajTelesford@gmail.com>
2024-10-21 19:42:48 -0500

    SCRUM-34 Forecasting displays on main page

--- server/ForecastService.py
+++ server/ForecastService.py
@@ -17,7 +17,7 @@ class ForecastService():
         params = {
             "latitude": latitude,
             "longitude": longitude,
-            "daily": ["cloudcover", "weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "uv_index_max", "uv_index_clear_sky_max", "precipitation_sum", "rain_sum", "showers_sum", "snowfall_sum", "precipitation_hours", "precipitation_probability_max", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum", "et0_fao_evapotranspiration"],
+            "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "uv_index_max", "uv_index_clear_sky_max", "precipitation_sum", "rain_sum", "showers_sum", "snowfall_sum", "precipitation_hours", "precipitation_probability_max", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum", "et0_fao_evapotranspiration"],
             "timezone": "auto",
             "models": "gfs_seamless"
         }
