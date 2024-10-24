66a470c36652dbceb9a53a940b45ecd3b066fb2a
TajTelesford <TajTelesford@gmail.com>
2024-10-24 14:46:04 -0500

    SCRUM-105 High Level Render Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import WeatherIconService from './WeatherIconService';
 import OpenWeatherIcon from './components/OpenWeatherIcon';
 import FeedbackForm from './components/FeedbackForm';
 import CurrentWeatherDataDisplay from './components/CurrentWeatherDataDisplay';
+import { RenderWeatherData } from './components/RenderWeatherData';
 
 
 // Functional component for the loading spinner
@@ -218,21 +219,20 @@ function App() {
           hasCityBeenEntered={setCityHasBeenEntered}
           />
         {/* Conditional rendering for loading spinner and weather data */}
-    <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : weatherData ? (
-        <> 
-          <CurrentWeatherDataDisplay weatherData={weatherData} />
-          {cityHasBeenEntered && <ForecastDisplay city={city} cityhasBeenEntered={cityHasBeenEntered} errorMessage={errorMessage} setError={setErrorMessage} />}
+    
+      
+        <RenderWeatherData  
+            weatherData={weatherData}
+            city={city}
+            cityHasBeenEntered={cityHasBeenEntered}
+            errorMessage={errorMessage}
+            setErrorMessage={setErrorMessage}
+            loading={loading}
+        />
+
           
-        </>
-        
-      ) : (
-        <p className="text-center">No weather data available.</p>
-      )}
-    </>
-  
+    
+   
         {/* Features Section */}
         <FeatureDisplaySection />
 
