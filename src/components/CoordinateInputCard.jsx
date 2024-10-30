215c9dc68afc5802bd300c4a7e20ffa3e787bdce
Anthony Rojas <ajr203@txstate.edu>
2024-10-30 13:09:46 -0500

    SCRUM-96 Added translation hook to CoordinateInputCard.jsx

--- src/components/CoordinateInputCard.jsx
+++ src/components/CoordinateInputCard.jsx
@@ -9,7 +9,7 @@ function CoordinateInputCard() {
   const [latCoordinates, setLatCoordinates] = useState();
   const [errorMessage, setErrorMessage] = useState('');
 
-  const { translatedText } = useTranslation();
+  const { translatedText } = useTranslation(); // Translation hook
   const navigate = useNavigate();
 
   const longHandleChange = (e) => {
@@ -42,11 +42,11 @@ function CoordinateInputCard() {
 
     return (
       <div className="max-w-md mx-auto mt-8 p-6 bg-white dark:bg-[#0f172a] rounded-lg shadow-lg">
-        <h2 className="text-2xl font-semibold mb-4 text-center dark:text-[#cbd5e1]">Enter Coordinates</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center dark:text-[#cbd5e1]">{translatedText.enterCoordinates}</h2>
   
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="latitude" className="block mb-2 dark:text-[#cbd5e1]">Latitude</label>
+            <label htmlFor="latitude" className="block mb-2 dark:text-[#cbd5e1]">{translatedText.Latitude}</label>
             <input
               type="text"
               name="latitude"
@@ -59,7 +59,7 @@ function CoordinateInputCard() {
           </div>
   
           <div className="mb-4">
-            <label htmlFor="longitude" className="block mb-2 dark:text-[#cbd5e1]">Longitude</label>
+            <label htmlFor="longitude" className="block mb-2 dark:text-[#cbd5e1]">{translatedText.Longitude}</label>
             <input
               type="text"
               name="longitude"
@@ -75,7 +75,7 @@ function CoordinateInputCard() {
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
   
           <button onClick={handleSubmit} type="submit" className="w-full py-2 px-4 bg-blue-600 text-white dark:bg-[#312e81] dark:hover:bg-[#4c1d95] rounded-lg">
-        Submit
+          {translatedText.Submit}
           </button>
         </form>
       </div>
