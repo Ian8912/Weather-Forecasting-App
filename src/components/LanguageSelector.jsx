5b920ef9a45dd15e6c8f1e3b83754e6704dfdcf1
Anthony Rojas <ajr203@txstate.edu>
2024-10-30 12:50:01 -0500

    SCRUM-96 Fixed UI issues

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -12,10 +12,15 @@ const LanguageSelector = () => {
       return (
         <div>
             <p className="mt-4 text-lg">Enter your perferred language.</p>
-            <select placeholder="English" value={language} onChange={handleChange}>
-            <option value="EN">English</option>
-            <option value="ES">Spanish</option>
-            <option value="FR">French</option>
+            <select 
+                placeholder="English" 
+                value={language} 
+                onChange={handleChange}
+                className="appearance-none px-3 py-2 border border-gray-300 rounded-md shadow-sm bg-white dark:bg-[#312e81] dark:text-white 
+                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-sm font-medium text-gray-700">
+                <option value="EN">English</option>
+                <option value="ES">Spanish</option>
+                <option value="FR">French</option>
             </select>
         </div>
       );
