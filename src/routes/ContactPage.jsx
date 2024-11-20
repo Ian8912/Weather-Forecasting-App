1e0f652087dc98319a9497130ac587e11a570c09
Anthony Rojas <ajr203@txstate.edu>
2024-11-19 19:04:33 -0600

    SCRUM-160 Added some styling to the contact page

--- src/routes/ContactPage.jsx
+++ src/routes/ContactPage.jsx
@@ -5,6 +5,11 @@ function ContactPage () {
     return (
         <div className="p-2 py-8 flex flex-col align-items-center bg-white dark:bg-[#0f172a]">
             <Navbar />
+            <div className="relative flex flex-col items-center justify-center min-h-screen bg-white text-black p-8">
+                <div className="absolute inset-0 bg-cover bg-center opacity-20"></div>
+                <div className="relative z-10 w-full max-w-5xl p-8 bg-blue-500 bg-opacity-90 rounded-lg shadow-lg">
+                </div>
+            </div>
         </div>
     )
 }
