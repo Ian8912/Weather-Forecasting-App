5ab69fffa5dda3ffe1a78ae80ce3b2a2349cd003
Anthony Rojas <ajr203@txstate.edu>
2024-11-25 11:39:59 -0600

    SCRUM-154 Added news route and link

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
       <BrowserRouter>
     
         <Routes>
-          
+          <Route path="/news" element={<NewsPage />} />
           <Route path="/" element={<App />} /> 
           <Route path="/coords/:lat/:long/" element={<WeatherPage />} />
           <Route path="/tailored-advice" element={<TailoredAdvice/>} />
