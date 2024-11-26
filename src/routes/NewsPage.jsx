5762d5700edbe6fdfbb454c71eb5b77071e619ba
Anthony Rojas <ajr203@txstate.edu>
2024-11-26 12:21:04 -0600

    SCRUM-156 Added fetch request for articles in the news page

--- src/routes/NewsPage.jsx
+++ src/routes/NewsPage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import Navbar from '../components/Navbar';
+import React, { useEffect, useState } from "react";
 
 function NewsPage() {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/weather-articles")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setArticles(data.articles || []);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+
     return(
         <div>
             <Navbar />
